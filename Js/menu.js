a54456559292a214642deffcbbd1e3d656302e07
abc <abc@gmail.com>
2022-11-02 10:42:33 -0500

    first commit

---
+++ Js/menu.js
@@ -0,0 +1,7 @@
+var btnMenu = document.getElementById('btn-menu');
+var nav = document.getElementById('nav');
+btnMenu.addEventListener('click', function(){
+    nav.classList.toggle('mostrar');
+})
+
+
